e44d3be7ad0039bea165f7795b3c539fd5c4b1f5
Joett Ocasio <joett_ocasio@Joetts-MacBook-Pro.local>
2023-01-15 15:57:22 -0800

    latest build

---
+++ pages/api/generate.js
@@ -0,0 +1,30 @@
+import { Configuration, OpenAIApi } from 'openai';
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
+const openai = new OpenAIApi(configuration);
+
+const basePromptPrefix = `
+create a pinescript strategy and backtest.
+
+//@version=4
+`;
+const generateAction = async (req, res) => {
+  // Run first prompt
+  console.log(`API: ${basePromptPrefix}${req.body.userInput}`)
+
+  const baseCompletion = await openai.createCompletion({
+    model: 'code-davinci-002',
+    prompt: `${basePromptPrefix}${req.body.userInput}\n`,
+    temperature: 0.5,
+    max_tokens: 400,
+  });
+  
+  const basePromptOutput = baseCompletion.data.choices.pop();
+
+  res.status(200).json({ output: basePromptOutput });
+};
+
+export default generateAction;
